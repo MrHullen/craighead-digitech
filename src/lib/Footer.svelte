cb942606002f90d05d1405a88abc44d21746de9c
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2022-01-13 10:51:22 +1300

    Initial commit

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Digital Technology</strong> by <a href="https://github.com/MrHullen">Mr Hullen</a>
+    </p>
+    <p>
+      See this page's source code <a href="">here</a>.
+    </p>
+    <p>
+      &copy; <a href="https://craighead.school.nz">Craighead Diocesan School</a>
+    </p>
+  </div>
+</footer>
