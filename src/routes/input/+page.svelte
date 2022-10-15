60b9e9abe29675ec5de288c89a7b65804fa8df06
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2022-10-15 21:03:10 +1300

    Upgrade to SvelteKit file-based routing

--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -6,16 +6,10 @@
 
 <main class="content section">
   <h1> Input </h1>
-  
-  <p>Try out one of these to help build your fluency with typing.</p>
 
   <div>
     <figure class="image is-16by9">
       <iframe class="has-ratio" src="https://apextyper.com/" frameborder="0" title="Apex Typer"></iframe>
-    </figure>
-
-    <figure class="image is-16by9">
-      <iframe class="has-ratio" src="https://warp-type.vercel.app/" frameborder="0" title="Warp Type"></iframe>
     </figure>
   </div>
 
@@ -29,12 +23,5 @@
   div {
     display: flex;
     justify-content: center;
-    flex-direction: column;
   }
-
-  /* iframe {
-    width: 80%;
-    height: 60vh;
-    margin: 1.5em auto;
-  } */
 </style>
