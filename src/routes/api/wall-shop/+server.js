2a001e21ac9e551cf471eda36c6979102a789874
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2024-01-30 13:06:53 +1300

    Updates API image links

--- src/routes/api/wall-shop/+server.js
+++ src/routes/api/wall-shop/+server.js
@@ -5,52 +5,52 @@ export function GET({ url }) {
     walls: [
       {
         name: 'Brick wall painted white',
-        img: url.href + '-images/white-brick.jpg',
+        img: url.origin + '/wall-shop-images/white-brick.jpg',
         price: 14.9,
       },
       {
         name: 'Brick wall painted pink',
-        img: url.href + '-images/pink-brick.jpg',
+        img: url.origin + '/wall-shop-images/pink-brick.jpg',
         price: 19.9,
       },
       {
         name: 'Unpainted brick wall',
-        img: url.href + '-images/unpainted-brick.jpg',
+        img: url.origin + '/wall-shop-images/unpainted-brick.jpg',
         price: 9.9,
       },
       {
         name: 'Plaster wall painted orange',
-        img: url.href + '-images/orange-plaster.jpg',
+        img: url.origin + '/wall-shop-images/orange-plaster.jpg',
         price: 11.9,
       },
       {
         name: 'Run-down wall painted yellow',
-        img: url.href + '-images/yellow-run-down.jpg',
+        img: url.origin + '/wall-shop-images/yellow-run-down.jpg',
         price: 4.9,
       },
       {
         name: 'Run-down wall painted grey',
-        img: url.href + '-images/grey-run-down.jpg',
+        img: url.origin + '/wall-shop-images/grey-run-down.jpg',
         price: 4.9,
       },
       {
         name: 'Run-down wall painted tan',
-        img: url.href + '-images/tan-run-down.jpg',
+        img: url.origin + '/wall-shop-images/tan-run-down.jpg',
         price: 4.9,
       },
       {
         name: 'Really rough wall',
-        img: url.href + '-images/rough.jpg',
+        img: url.origin + '/wall-shop-images/rough.jpg',
         price: 4.9,
       },
       {
         name: 'Wall with flowers',
-        img: url.href + '-images/flowers.jpg',
+        img: url.origin + '/wall-shop-images/flowers.jpg',
         price: 24.9,
       },
       {
         name: 'Art-deco wall',
-        img: url.href + '-images/art-deco.jpg',
+        img: url.origin + '/wall-shop-images/art-deco.jpg',
         price: 34.9,
       },
     ],
