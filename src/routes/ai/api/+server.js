2e339376c5aa99251de5c279270ea1a6b1462821
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2024-05-28 12:16:21 +1200

    Adds FDR (again)

--- src/routes/ai/api/+server.js
+++ src/routes/ai/api/+server.js
@@ -1,25 +1,26 @@
 import { error } from '@sveltejs/kit'
+import { createOpenAI } from '@ai-sdk/openai'
 import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { StreamingTextResponse, streamText } from 'ai'
 
 import { env } from '$env/dynamic/private'
 
 // Create an OpenAI API client
-const openai = new OpenAI({
+const openai = createOpenAI({
   apiKey: env.OPENAI_API_KEY || '',
 })
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge'
 
-export async function POST({ request }) {
+export const POST = async ({ request }) => {
   try {
     // Extract the `prompt` from the body of the request
     const { messages } = await request.json()
 
     // Ask OpenAI for a streaming chat completion given the prompt
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4o',
+    const response = await streamText({
+      model: openai('gpt-4o'),
       stream: true,
       max_tokens: 2000,
       messages: messages.map((message) => ({
@@ -28,16 +29,15 @@ export async function POST({ request }) {
       })),
     })
 
-    // Convert the response into a friendly text-stream
-    const stream = OpenAIStream(response)
-    // Respond with the stream
-    return new StreamingTextResponse(stream)
+    // Convert the response into a friendly text-stream and return it
+    return new StreamingTextResponse(response.toAIStream())
   } catch (err) {
     // Check if the error is an APIError
     if (err instanceof OpenAI.APIError) {
       const { name, status, headers, message } = err
       throw error(420, 'Your AI had this to say: ' + 'Status: ' + status + ' | Message: ' + message)
     } else {
+      console.error('An error occurred while processing the request: ', err)
       throw error
     }
   }
