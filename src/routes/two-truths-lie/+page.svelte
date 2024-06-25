2dd0d7055df7b9c9136a6c6f8b81d376461f46e1
Mr. Hullen <48824760+MrHullen@users.noreply.github.com>
2024-06-26 11:27:42 +1200

    Add two truths and a lie

---
+++ src/routes/two-truths-lie/+page.svelte
@@ -0,0 +1,9 @@
+
+<iframe src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/Jx5udahoxNL65A3Lq6aMVB/Year-10-DigiTech?node-id=0-1&t=8rNQDHcFX0V7C3D1-1" allowfullscreen></iframe>
+
+<style>
+  iframe {
+    width: 1280px;
+    height: 832px;
+  }
+</style>
